eeebeb6f0c57b8c3b5427cbb4b28b2ffbda8cca2
NicholasLee76 <53105028+NicholasLee76@users.noreply.github.com>
2022-07-20 14:07:07 -0400

    Add files via upload

---
+++ v4/edge.py
@@ -0,0 +1,22 @@
+class Edge:
+    # initializes edge instance
+    def __init__(self, src, dest, name, weight):
+        self.src = src  # starting point, Node type
+        self.dest = dest  # ending point, Node type
+        self.name = name  # name, String type
+        self.weight = weight  # weight, Boolean type
+
+    def get_src(self):
+        return self.src
+
+    def get_dest(self):
+        return self.dest
+
+    def get_weight(self):
+        return self.weight
+
+    def get_name(self):
+        return self.name
+
+    def set_weight(self, new):
+        self.weight = new
