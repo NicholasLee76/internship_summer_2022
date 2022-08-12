1537c5517f4272658faf0d1c89663a6a53656d0f
NicholasLee76 <53105028+NicholasLee76@users.noreply.github.com>
2022-08-12 14:01:41 -0400

    Update single_pressure_graphing.py

--- hardware_control/single_pressure_graphing.py
+++ hardware_control/single_pressure_graphing.py
@@ -8,14 +8,13 @@ from PyExpLabSys_master.PyExpLabSys.drivers.xgs600 import XGS600Driver
 
 start = time.time()
 x = []
-backup_q = queue.Queue()
 mmbe_q = queue.Queue()
 
 
-def read_gauge(q, gauge):
+def read_gauge(q, gauge, num):
     while True:
-        lower_pressure = gauge.read_all_pressures()[2]
-        q.put(lower_pressure)
+        pressure = gauge.read_all_pressures()[num]
+        q.put(pressure)
 
 
 def get_val(q):
@@ -24,31 +23,33 @@ def get_val(q):
 
 def plot_cont(func, param, xmax):
     y = []
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
+    fig, ax = plt.subplots()
 
     def update(i):
         yi = func(param)
         y.append(yi)
         x.append(int(time.time()-start))
         ax.clear()
+        ax.annotate(f'pressure: {y[-1]}', xycoords='figure fraction', xy=(0.15, 0.8))
+        plt.yscale('log')
+        plt.ylim(1 * 10**-9, 1.5*max(y))
         ax.plot(x, y)
         ax.set_xlabel('elapsed time (s)')
         ax.set_ylabel('pressure (torr)')
-        ax.set_title(f'backup chamber pressure ({date.today()})')
+        ax.set_title(f'mmbe chamber pressure ({date.today()})')
         print(f"{i}: {yi} @ {x[-1]} seconds")
-        time.sleep(0.5)
+        time.sleep(0.1)
 
     a = anim.FuncAnimation(fig, update, frames=xmax, repeat=False)
     plt.show()
 
 
 def main(s):
-    g1 = XGS600Driver(port='/dev/cu.usbserial-110') #replace with appropriate comm port for windows
+    g1 = XGS600Driver(port='/dev/cu.usbserial-210') #replace with appropriate comm port for windows
 
-    threading.Thread(target=read_gauge, args=(backup_q, g1, ), daemon=True).start()
+    threading.Thread(target=read_gauge, args=(mmbe_q, g1, 2), daemon=True).start()
 
-    plot_cont(get_val, backup_q, s)
+    plot_cont(get_val, mmbe_q, s)
 
 
 if __name__ == '__main__':
